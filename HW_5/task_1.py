3418941959043e07e3e632e8ce67d94e539996d0
elenakrivobokova <123370851+elenakrivobokova@users.noreply.github.com>
2023-04-25 19:56:02 +0300

    Update HW_5/task_1.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -9,7 +9,7 @@
 def exp(a, b):
    if b == 0:
       return 1
-   return (a * exp(a, b - 1))
+   return a * exp(a, b - 1)
     
 num_1= int(input('Введите число A: '))
 num_2 = int(input('Введите число B: '))
