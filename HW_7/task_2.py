5474e2fd615649b60cbe3293cc534c4aae4a1f7f
Elena K <lena-k861@yandex.ru>
2023-05-24 13:23:11 +0300

    the task #2 from HW_7 was modified

--- HW_7/task_2.py
+++ HW_7/task_2.py
@@ -14,9 +14,9 @@
 #  5 10 15 20 25 30
 #  6 12 18 24 30 36
 
-def print_operation_table(operation, num_rows, num_columns):
+def print_operation_table(operation, num_rows = 6, num_columns = 6):
     for row in range(1, num_rows + 1):
         for column in range(1, num_columns + 1):
             print(operation(row,column), end='\t')
         print()
-print_operation_table(lambda x,y: x*y, 6, 6)
+print_operation_table(lambda x,y: x*y)
